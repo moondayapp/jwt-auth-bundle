23cd21cb0bb8ce8870a7559aaa9a2af2b4237583
Olivier Delbruyere <odelbruyere@moondayapp.com>
2019-10-22 15:22:16 +0200

    fix duplicate use

--- src/Security/Core/JWTUserProviderInterface.php
+++ src/Security/Core/JWTUserProviderInterface.php
@@ -6,7 +6,6 @@ use stdClass;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @author german
